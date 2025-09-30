ad5bb9105363c139f9a43c728d0fc52b04de6b80
pshingsl <w455ok@gmail.com>
2025-09-30 17:11:18 +0900

    feat: 카테고리 컨트롤러

--- src/main/java/com/bidnbuy/server/entity/CategoryEntity.java
+++ src/main/java/com/bidnbuy/server/entity/CategoryEntity.java
@@ -6,18 +6,32 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
-@Table(name = "Categorys")
+@Table(name = "Category")
 public class CategoryEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "category_id")
     private Long categoryId;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private CategoryEntity parent;
+
+    // 하위 카테고리 중분류
+    @OneToMany(mappedBy = "parent")
+    private List<CategoryEntity> children = new ArrayList<>();
+
     @Column(name = "category_name", nullable = false)
     private String categoryName;
+
+    @OneToMany(mappedBy = "category")
+    private List<AuctionProductsEntity> auctionProducts = new ArrayList<>();
 }
