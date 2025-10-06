1114b7f0a99a7586408e2948528efb32e10895a2
pshingsl <w455ok@gmail.com>
2025-10-07 04:14:16 +0900

    feat: 경매 결과

---
+++ src/main/java/com/bidnbuy/server/entity/OrderEntity.java
@@ -0,0 +1,15 @@
+package com.bidnbuy.server.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class OrderEntity {
+}
