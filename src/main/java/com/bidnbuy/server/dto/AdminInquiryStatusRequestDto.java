936e6da31e96dc543e849e8d82e255304588407d
MA-Ha-eun <1thecalla@gmail.com>
2025-11-02 01:30:19 +0900

    docs: AdminInquiryListDto, AdminInquiryDetailDto, AdminInquiryReplyRequestDto, AdminInquiryStatusRequestDto 에 @Schema 추가

--- src/main/java/com/bidnbuy/server/dto/AdminInquiryStatusRequestDto.java
+++ src/main/java/com/bidnbuy/server/dto/AdminInquiryStatusRequestDto.java
@@ -1,6 +1,7 @@
 package com.bidnbuy.server.dto;
 
 import com.bidnbuy.server.enums.InquiryEnums;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,8 +12,10 @@ import jakarta.validation.constraints.NotNull;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "관리자 문의 답변 상태 변경")
 public class AdminInquiryStatusRequestDto {
     
     @NotNull(message = "상태는 필수입니다")
+    @Schema(description = "변경하려는 문의 답변 상태", example = "WAITING", required = true)
     private InquiryEnums.InquiryStatus status;
 }
