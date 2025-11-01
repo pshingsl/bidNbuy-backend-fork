af80c9ee875a95acb604ba390dd765af9a6f733d
MA-Ha-eun <1thecalla@gmail.com>
2025-11-02 03:28:33 +0900

    docs: 페널티 관련 swagger 추가

--- src/main/java/com/bidnbuy/server/dto/PenaltyRequestDto.java
+++ src/main/java/com/bidnbuy/server/dto/PenaltyRequestDto.java
@@ -1,5 +1,6 @@
 package com.bidnbuy.server.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +10,11 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "페널티 부과")
 public class PenaltyRequestDto {
+    @Schema(description = "페널티 부과 유저 ID", example = "1", required = true)
     private Long userId;
-    private String type; // LEVEL_1, LEVEL_2, LEVEL_3
+    
+    @Schema(description = "페널티 타입 (LEVEL_1: 10점, LEVEL_2: 30점, LEVEL_3: 50점)", example = "LEVEL_1", required = true, allowableValues = {"LEVEL_1", "LEVEL_2", "LEVEL_3"})
+    private String type;
 }
