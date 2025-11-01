ac10acd66b13e71557f6c4e9fb9d73de156e5ef5
MA-Ha-eun <1thecalla@gmail.com>
2025-11-01 23:56:44 +0900

    docs: AdminSignupRequestDto, AdminLoginRequestDto에 @Schema 추가

--- src/main/java/com/bidnbuy/server/dto/AdminSignupRequestDto.java
+++ src/main/java/com/bidnbuy/server/dto/AdminSignupRequestDto.java
@@ -1,5 +1,6 @@
 package com.bidnbuy.server.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +11,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AdminSignupRequestDto {
+    @Schema(description = "관리자 이메일", example = "admin@bidnbuy.com", required = true)
     private String email;
+    
+    @Schema(description = "관리자 비밀번호 (영문, 숫자 포함 8자리 이상)", example = "admin123", required = true)
     private String password;
+    
+    @Schema(description = "관리자 닉네임", example = "관리자1", required = true)
     private String nickname;
+    
+    @Schema(description = "IP 수집 동의 여부", example = "true", required = true)
     private Boolean ipConsentAgreed;
 }
